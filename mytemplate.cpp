fb624c3d95a01a0edaf21912c24cd618c2245a28
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-08 18:11:40 +0530

    Added my template for programming contests

---
+++ mytemplate.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF=int(1e9);
+const ll INF64=(ll)(1e18);
+const ld EPS=1e-9;
+const ld PI=3.1415926535897932384626433832795;
+
+
+int main(){
+	
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);  
+
+    
+
+
+
+
+
+
+	return 0;
+} 
