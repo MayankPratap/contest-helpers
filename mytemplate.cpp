d02d736f0734a731ee44518aa9f78eef6778c987
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-21 17:35:54 +0530

    Removed extra headers and added ASCII ART

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,24 +1,39 @@
+/*  
+    This code is powered by ASCII ART and C++. 
+
+    _._ _..._ .-',     _.._(`))
+'-. `     '  /-._.-'    ',/
+   )         \            '.
+  / _    _    |             \
+ |  a    a    /              |
+ \   .-.                     ;  
+  '-('' ).-'       ,'       ;
+     '-;           |      .'
+        \           \    /
+        | 7  .__  _.-\   \
+        | |  |  ``/  /`  /
+       /,_|  |   /,_/   /
+          /,_/      '`-'
+          
+   ....   Please give me AC   ......  
+
+*/
 #include<bits/stdc++.h>
-#include<ext/pb_ds/detail/standard_policies.hpp>
-#include<ext/pb_ds/assoc_container.hpp> // Common file
-#include<ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-using namespace __gnu_pbds;
 using namespace std;
 #define ff first
 #define ss second
 #define pb push_back
 #define mp make_pair
 
+#define MOD 1000000007
+
 typedef long long ll;
 typedef long double ld;
 
-const int INF=int(1e9);
+const int INF=(int)(1e9);
 const ll INF64=(ll)(1e18);
 const ld EPS=1e-9;
 const ld PI=3.1415926535897932384626433832795;
-
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
-ordered_set X;
 
 int main(){
 	
