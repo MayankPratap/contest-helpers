bbc22dc2854ab35ba8eef33cd49650cf727c02ae
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-05-22 11:41:01 +0530

    Added typedef for std::priority_queue

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,7 +1,6 @@
-/*  
+/*
    Mayank Pratap Singh
    MNNIT, 2nd year IT
-         
    AC ho.
 */
 #include<bits/stdc++.h>
@@ -20,15 +19,16 @@ const int INF=(int)(1e9);
 const ll INF64=(ll)(1e18);
 const ld EPS=1e-9;
 const ld PI=3.1415926535897932384626433832795;
-
-
 typedef vector<int> vi;
 typedef vector<pair<int,int> > vii;
 typedef vector<list<int> > vl;
 typedef vector<list<pair<int,int> > > vlp;
+typedef vector<list<pair<int,double> > >vld;  
 typedef map<int,int> mi;
 typedef map<string,int> ms;
 typedef set<int> si;
+typedef priority_queue<pair<double,int>,vector<pair<double,int> >,greater<pair<double,int> > > queue_double;
+typedef priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > queue_int;
 
 
 int main(){
